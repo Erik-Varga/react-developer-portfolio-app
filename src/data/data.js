3d3bcfb0afcdf2d7975c88867be1807a7559e0d1
Erik Varga <viglew@gmail.com>
2024-08-26 13:35:17 -0400

    fixed img ref

--- src/data/data.js
+++ src/data/data.js
@@ -3,14 +3,14 @@ import GarageSaleImg from '../assets/projects/img_garage_sale_app.png'
 import ShutTheBoxImg from '../assets/projects/img_shut_the_box_js_game.png'
 import AsteroidsImg from '../assets/projects/img_asteroids_game.png'
 import PomodoroImg from '../assets/projects/img_pomodoro_timer_js_app.png'
-import PowerBISoundMan from '../assets/projects/img_powerbi_soundman_dashboard.png'
+import PowerBISoundManImg from '../assets/projects/img_powerbi_soundman_dashboard.png'
 
 export const items = [
     {
         id: 1,
         name:"SoundMan Studio Dashboard",
         category: 'Power BI',
-        image: GarageSaleImg,
+        image: PowerBISoundManImg,
         github:"https://github.com/Erik-Varga/powerbi-soundman-studio-dashboard",
         live:"https://app.powerbi.com/groups/me/reports/a2a63e2c-ba62-4c9d-b55d-79d8f35d6721/df628c0e9a88c0823136?experience=power-bi",
     },
