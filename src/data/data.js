9241b0b52bcdfb8f01d7a6e72495c47a937137e3
Erik Varga <viglew@gmail.com>
2024-08-26 13:32:51 -0400

    added powerbi project

--- src/data/data.js
+++ src/data/data.js
@@ -3,10 +3,19 @@ import GarageSaleImg from '../assets/projects/img_garage_sale_app.png'
 import ShutTheBoxImg from '../assets/projects/img_shut_the_box_js_game.png'
 import AsteroidsImg from '../assets/projects/img_asteroids_game.png'
 import PomodoroImg from '../assets/projects/img_pomodoro_timer_js_app.png'
+import PowerBISoundMan from '../assets/projects/img_powerbi_soundman_dashboard.png'
 
 export const items = [
     {
         id: 1,
+        name:"SoundMan Studio Dashboard",
+        category: 'Power BI',
+        image: GarageSaleImg,
+        github:"https://github.com/Erik-Varga/powerbi-soundman-studio-dashboard",
+        live:"https://app.powerbi.com/groups/me/reports/a2a63e2c-ba62-4c9d-b55d-79d8f35d6721/df628c0e9a88c0823136?experience=power-bi",
+    },
+    {
+        id: 2,
         name:"Garage Sale App",
         category: 'React JS',
         image: GarageSaleImg,
@@ -14,7 +23,7 @@ export const items = [
         live:"https://react-private-garage-sale-app.vercel.app",
     },
     {
-        id: 2,
+        id: 3,
         name:"Wordle Game",
         category: 'React JS',
         image: WordleImg,
@@ -22,7 +31,7 @@ export const items = [
         live:"https://react-wordle-game-beige.vercel.app/",
     },
     {
-        id: 3,
+        id: 4,
         name:"Pomodoro Timer App",
         category: 'React JS',
         image: PomodoroImg,
@@ -30,7 +39,7 @@ export const items = [
         live:"https://react-pomodoro-timer-app.vercel.app/",
     },
     {
-        id: 4,
+        id: 5,
         name:"Shut The Box Game",
         category: 'JavaScript',
         image: ShutTheBoxImg,
@@ -38,7 +47,7 @@ export const items = [
         live:"https://erik-varga.github.io/js-shut-the-box/",
     },
     {
-        id: 5,
+        id: 6,
         name:"Asteroids Game",
         category: 'JavaScript',
         image: AsteroidsImg,
