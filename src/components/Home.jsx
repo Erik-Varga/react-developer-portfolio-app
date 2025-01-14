4cee40a38dd5db79c4c96a5ea04b2b82f738556a
Erik Varga <viglew@gmail.com>
2025-01-13 21:25:49 -0500

    updated home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-scroll";
 import Social from './Social';
 import '../index.css';
 
+import { HiMiniCircleStack } from 'react-icons/hi2';
+import { LiaBroomSolid } from 'react-icons/lia';
+import { PiFunnel } from 'react-icons/pi';
+import { BsBarChartLine } from 'react-icons/bs';
+import { WindowSize } from './WindowSize';
+import { CgDrive } from 'react-icons/cg';
+import { BiRightArrow, BiRightArrowAlt } from 'react-icons/bi';
+import { GiCycle } from 'react-icons/gi';
+
 const Home = () => {
   return (
     <div name="home" className="relative md:h-screen w-full bg-primary dark:bg-dark-primary text-white dark:text-black z-10">
@@ -16,9 +25,16 @@ const Home = () => {
           Research, Developer,
         </h2>
         <h2 className="text-4xl sm:text-7xl font-bold text-color2">
-          Data Analyst
+           Analyst
         </h2>
         <p className="text-color2 py-4 max-w-[700px]">
+        <div className='flex flex-col md:flex-row items-center gap-5 my-5 text-white font-bold border border-gray-500 p-5 rounded-sm'>
+          <div className='flex items-center gap-3'>DATA <GiCycle className='text-3xl' /></div>
+          <HiMiniCircleStack className='text-5xl hover:text-green-400'/> Gathering         
+          <LiaBroomSolid className='text-5xl hover:text-green-400'/> Cleaning          
+          <PiFunnel className='text-5xl hover:text-green-400'/> Preprocessing 
+          <BsBarChartLine className='text-5xl hover:text-green-400'/> Visualization 
+        </div>
         I am a research developer experienced in both frontend and backend technologies. I enjoy learning new skills, using new coding tools, building and sharing code solutions with the developer community.
         </p>
         <div>
