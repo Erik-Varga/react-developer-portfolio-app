9fbbb194ff78ca9f8bfbc0773552ea969d9faabf
Erik Varga <viglew@gmail.com>
2025-02-03 19:50:15 -0500

    updated home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
   const size = 20;
 
   return (
-    <div name='about' className='w-full md:h-screen'>
+    <div name='about' className='w-full md:h-screen py-10'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
           <div className='sm:text-right pb-8 pl-4'>
